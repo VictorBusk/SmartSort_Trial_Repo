93b4118a68d57fad8605d9dd39e143bf62f7482a
Unknown <mbrbayard@live.dk>
2016-10-29 11:11:44 +0200

    lol
    lol

--- SmartSort/Rule.cs
+++ SmartSort/Rule.cs
@@ -10,20 +10,22 @@ namespace SmartSort
 {
     public class Rule
     {
-        public string name { get; set; }
-        public string source { get; set; }
-        public string destination { get; set; }
-        public string keyWord { get; set; }
+        public String name { get; set; }
+        public String source { get; set; }
+        public String destination { get; set; }
+        public String keyWord { get; set; }
         public bool includeFolders { get; set; }
         public bool FoldersNeedKey { get; set; }
-        public string fileExtension { get; set; }
+        public String fileExtension { get; set; }
         public bool keepSource { get; set; }
         public bool replaceFile { get; set; }
         public bool allFilesInKeyFolder { get; set; }
-        private string destination_temp;
-        private string source_temp;
+        private String destination_temp;
+        private String source_temp;
+        private bool startingWith {get; set;}
+        private bool endingWith { get; set; }
 
-        public Rule(string _name, string _source, string _destination, string _keyWord, string _fileExtension, bool _includeFolders, bool _FoldersNeedKey, bool _keepSource, bool _replaceFile, bool _allFilesInKeyFolder)
+        public Rule(String _name, String _source, String _destination, String _keyWord, String _fileExtension, bool _includeFolders, bool _FoldersNeedKey, bool _keepSource, bool _replaceFile, bool _allFilesInKeyFolder, bool _startingWith, bool _endingWith)
         {
             name = _name;
             source = _source;
@@ -37,60 +39,81 @@ namespace SmartSort
             FoldersNeedKey = _FoldersNeedKey;
             replaceFile = _replaceFile;
             allFilesInKeyFolder = _allFilesInKeyFolder;
+            startingWith = _startingWith;
+            endingWith = _endingWith;
         }
 
         public void executeRule()
         {
             SearchDirectory(source);
         }
-
-        private void SearchDirectory(string dir)
+        private void iterate(String d)
+        {
+            source_temp = d.Replace(source, "");
+            destination_temp = destination + source_temp;
+            Directory.CreateDirectory(destination_temp);
+            SearchDirectory(d);
+        }
+        public bool InBeginning(String path)
+        {
+            if (startingWith)
+            {
+                Console.WriteLine(path.Substring(path.LastIndexOf(@"\") + 1));
+                return path.Substring(path.LastIndexOf(@"\") + 1).StartsWith(keyWord);
+            }
+            return false;
+        }
+        public bool InEnd(String path)
+        {
+            if (endingWith)
+            {
+                Console.WriteLine(path.Substring(path.LastIndexOf(@"\") + 1, path.LastIndexOf('.') - (path.LastIndexOf(@"\") + 1)));
+                return path.Substring(path.LastIndexOf(@"\") + 1, path.LastIndexOf('.') - (path.LastIndexOf(@"\") + 1)).EndsWith(keyWord);
+            }
+            return false;
+        }
+        private void SearchDirectory(String dir)
         {
             try
             {
-                foreach (string f in Directory.GetFiles(dir))
+                foreach (String f in Directory.GetFiles(dir))
                 {
-                    if (f.Substring(f.LastIndexOf(@"\")).Contains(keyWord) && f.Substring(f.LastIndexOf(@"\")).Contains(fileExtension))
+                    if (f.Substring(f.LastIndexOf('\\')).Contains(keyWord) && f.Substring(f.LastIndexOf('.')).Contains(fileExtension) || InBeginning(f) || InEnd(f))
                     {
                         moveFile(f);
                     }
-                    else if (allFilesInKeyFolder && f.Substring(f.LastIndexOf(@"\")).Contains(fileExtension))
+                    else if (allFilesInKeyFolder && f.Substring(f.LastIndexOf('.')).Contains(fileExtension))
                     {
                         moveFile(f);
                     }
                 }
                 if (includeFolders)
                 {
-                    foreach (string d in Directory.GetDirectories(dir))
+                    foreach (String d in Directory.GetDirectories(dir))
                     {
+                        //contain here need to be fixed!!
                         if (FoldersNeedKey == true && d.Contains(keyWord))
                         {
-                            source_temp = d.Replace(source, "");
-                            destination_temp = destination + source_temp;
-                            Directory.CreateDirectory(destination_temp);
-                            SearchDirectory(d);
+                            iterate(d);
                         }
                         else
                         {
-                            source_temp = d.Replace(source, "");
-                            destination_temp = destination + source_temp;
-                            Directory.CreateDirectory(destination_temp);
-                            SearchDirectory(d);
+                            iterate(d);
                         }
                     }
                 }
             }
             catch (System.Exception) { }
         }
-        private void moveFile(string path)
+        private void moveFile(String path)
         {
             var fileDestination = destination + path.Replace(source, "");
-            string filename = fileDestination.Substring(path.LastIndexOf(@"\"));
+            String filename = fileDestination.Substring(path.LastIndexOf(@"\"));
             char left = '[', right = ']';
             if (filename.Contains(left) && filename.Contains(right))
             {
-                string folderpath = fileDestination.Substring(0, path.LastIndexOf(@"\"));
-                string newfolder = inbetween(filename, left.ToString(), right.ToString());
+                String folderpath = fileDestination.Substring(0, path.LastIndexOf(@"\"));
+                String newfolder = inbetween(filename, left.ToString(), right.ToString());
                 if (!Directory.Exists(folderpath + newfolder))
                 {
                     Directory.CreateDirectory(folderpath + newfolder);
@@ -100,20 +123,11 @@ namespace SmartSort
             }
             if (keepSource)
             {
-                if (!replaceFile)
-                {
-                    fileDestination = fileDestination.Insert(fileDestination.LastIndexOf("."), "(" + DateTime.Now.ToString("ddMMyyyyHHmmss") + ")");
-                    Console.WriteLine(fileDestination);
-                }
+                unique(fileDestination);
                 File.Copy(path, fileDestination, replaceFile);
             }
             else {
-                if (!replaceFile)
-                {
-                    fileDestination = fileDestination.Insert(fileDestination.LastIndexOf("."), "(" + DateTime.Now.ToString("ddMMyyyyHHmmss") + ")");
-                    Console.WriteLine(fileDestination);
-                }
-                else {
+                if(!unique(fileDestination)) {
                     if (File.Exists(fileDestination))
                     {
                         File.Delete(fileDestination);
@@ -122,17 +136,31 @@ namespace SmartSort
                 File.Move(path, fileDestination);
             }
         }
-        private string inbetween(string source, string first, string last)
+        private bool unique(String fileDestination)
         {
-            string st = source;
+            if (!replaceFile)
+            {
+                fileDestination = fileDestination.Insert(fileDestination.LastIndexOf("."), "(" + DateTime.Now.ToString("ddMMyyyyHHmmss") + ")");
+                Console.WriteLine(fileDestination);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private String inbetween(String source, String first, String last)
+        {
+            String st = source;
             int pFrom = st.IndexOf(first) + first.Length;
             int pTo = st.LastIndexOf(last);
             return st.Substring(pFrom, pTo - pFrom);
         }
-        private string RemoveBetween(string s, char begin, char end)
+        private String RemoveBetween(String s, char begin, char end)
         {
             Regex regex = new Regex(string.Format("\\{0}.*?\\{1}", begin, end));
             return regex.Replace(s, string.Empty);
         }
+        
     }
 }
